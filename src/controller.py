4788ad7f9ebdbdb9f1d135ff2e6467d48a543671
Katarzyna Mazur <mazur66@hotmail.com>
2023-11-24 21:18:47 -0500

    Moved user input to controller, coded user method and mainloop.

--- src/controller.py
+++ src/controller.py
@@ -1,9 +1,41 @@
-from user import User
-from proxy import Proxy
-from button import Button
+from src.user import User
+#from proxy import Proxy
+#from button import Button
+import pygame
 
-class Controller():
+class Controller:
     '''
     To control the program
     '''
-    pass
+    def __init__(self):
+        #screen = pygame.display.get_mode()
+        pass
+        
+
+    def get_user(self):
+        month = (input("What's your birthday month (january, february,.): "))
+        day = int(input("What's your birthday day: "))
+        #print(month,day)
+        return month,day
+    
+    def mainloop(self):
+        print("checkpoint -2")
+        cont = Controller()
+        print("checkpoint -1")
+        [month,day] = cont.get_user()
+        print(month,day)
+        print("checkpoint 0")
+        user = User(month, day)
+        print("checkpoint 1")
+        z = user.zodiac_sign() 
+        print("checkpoint 2")
+        print(z)
+        print("checkpoint 3")
+
+#Controller.mainloop()
+
+# in a method in Controller class
+# need to create User object before
+# p = Proxy()
+# horo = p.get_horo()
+# a = user.zodiac_sign() 
