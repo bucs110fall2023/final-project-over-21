8ffff8732ba2a615b91e4f6e422aac86a44f8dc9
Katarzyna Mazur <mazur66@hotmail.com>
2023-11-25 01:01:52 -0500

    Worked on proxy - still not taking zodiac sign arg

--- src/proxy.py
+++ src/proxy.py
@@ -14,7 +14,7 @@ class Proxy:
         querystring = {"s":"z"}
 
         headers = {
-        "X-RapidAPI-Key": "39d1c4f37bmshcdfe6dac9622395p1dc660jsn6a4727b5182a",
+        "X-RapidAPI-Key": 
         "X-RapidAPI-Host": "horoscope-astrology.p.rapidapi.com"
         }
         response = requests.get(self.url, headers=headers, params=querystring)
