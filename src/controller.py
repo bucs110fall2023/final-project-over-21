efd5b1da21deff6e2dba6fc4a3ec3246569723d0
Katarzyna Mazur <mazur66@hotmail.com>
2023-12-02 11:52:45 -0500

    Moved code for creating input to separate file.

--- src/controller.py
+++ src/controller.py
@@ -21,65 +21,7 @@ class Controller:
         text = font.render("Enter your birthday month, day to display your Zodiac info: ", True, "black")
         screen.blit(text, (100, 100))
         pygame.display.flip()
-        
-        # credit: YouTube tutorial: Python/Pygame tutorial: Getting text input by Clear Code
-        # steps for creating text input:
-        # 1 . create a text font, 2. render text with the font 3, display rendered text
-
-        #set up
-        #pygame.init()
-        # clock = pygame.time.Clock()
-        # screen = pygame.display.set_mode()
-        # screen.fill("pink")
-        # base_font = pygame.font.Font(None,42)
-        # user_text = ""
-
-        # # Creating a rectangle for input
-        # input_rect = pygame.Rect(200, 200, 240,50)
-        # #color = pygame.Color("yellow")
-        # # creating variables that will be used for selecting and disselecting the rectangle
-        # color_active = pygame.Color("yellow")
-        # color_passive = pygame.Color("blue")
-        # color = color_passive
-
-        # active = False
-    
-        # while True:
-        #     for event in pygame.event.get():
-        #         if event.type == pygame.QUIT:
-        #             pygame.quit()
-        #             sys.exit()
-                
-        #         # checking if user's mouse is inside the box/rectangle    
-        #         if event.type == pygame.MOUSEBUTTONDOWN:
-        #             if input_rect.collidepoint(event.pos):
-        #                 active = True
-        #             else:
-        #                 active = False
-                
-        #         # getting input from the user, BACKSPACE IS NOT WORKING!    
-        #         if event.type == pygame.KEYDOWN:
-        #             if active == True:
-        #                 if event.key == pygame.K_BACKSPACE:
-        #                     user_text = user_text[:-1]
-        #                 else:
-        #                     user_text += event.unicode
-        #                     print(user_text)
-        #                     return user_text
-        #     if active:
-        #         color = color_active
-        #     else:
-        #         color = color_passive
-            
-        #     pygame.draw.rect(screen,color,input_rect,2)
-            
-        #     # rendering text on its own surface   
-        #     text_surface = base_font.render(user_text,True,(255,255,255))
-        #     screen.blit(text_surface,(input_rect.x + 5, input_rect.y + 5))
-                
-        #     pygame.display.flip()
-        #     clock.tick(60)        
-        
+       
     def get_birthday(self):
         month = (input("What's your birthday month (january, february, etc.): "))
         # testing the entry if it is name of a month
@@ -98,9 +40,10 @@ class Controller:
     
     def mainloop(self):
         print("checkpoint -2")
-        controller = Controller()
+        #controller = Controller()
         print("checkpoint -1")
-        [month, day] = controller.get_birthday()
+        #[month, day] = controller.get_birthday()
+        [month, day] = self.get_birthday()
         print(month, day)
         print("checkpoint 0")
         user = User(month, day)
