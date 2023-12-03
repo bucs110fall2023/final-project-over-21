928af8a703316b2794b89a520ab4d4d7c70a5677
Anne Peng <aepeng@gmail.com>
2023-12-03 09:16:31 -0500

    WIP Testing pygame-menu in controller.py

--- src/button.py
+++ src/button.py
@@ -1,11 +1,27 @@
-class Button():
+import pygame
+
+class Button(pygame.sprite.Sprite):
     '''
-    button to enter another birthday
+    Button class to create a Submit button and a Play Again button
     '''
-    pass
+    def __init__(self, x=0, y=0, width=150, height=150, color="red", text="Submit"):
+        super().__init__()
+        self.image = pygame.Surface(width, height)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.color = color
+        self.image.fill(self.color)
 
-# Testing this comment!!
+        self.text = pygame.font.SysFont(None, 36).render(text, True, "black")
 
-# Testing comment number 2
+        self.image.blit(self.message, (20, 20))
 
-# This is branch1 and NOT the master
+
+    def button_hover(self, color):
+        self.color = color
+        self.image.fill(self.color)
+
+
+    def button_click(self):
+        pass
--- src/controller.py
+++ src/controller.py
@@ -1,10 +1,11 @@
 import pygame
-from src.user import User
-from src.proxy import Proxy
+import pygame_menu
+# from src.user import User
+# from src.proxy import Proxy
 
 # For testing mainloop() method within controller.py
-# from user import User
-# from proxy import Proxy
+from user import User
+from proxy import Proxy
 
 
 class Controller:
@@ -12,20 +13,30 @@ class Controller:
     To control the program
     '''
     def __init__(self):
-        pass
-        screen = pygame.display.set_mode()
-        screen.fill("pink")
+        # For testing mainloop() method within controller.py
+        pygame.init()
+
+        self.screen = pygame.display.set_mode()
+        self.screen.fill("pink")
         
         font = pygame.font.Font(None, 35)
         text = font.render("Enter your birthday month, day to display your Zodiac info: ", True, "black")
-        screen.blit(text, (100, 100))
+        self.screen.blit(text, (100, 100))
+
+        # Previous code
+        start_menu = pygame_menu.Menu("Read your horoscope for today", width=400, height=300, theme=pygame_menu.themes.THEME_SOLARIZED)
+        start_menu.add.text_input("Birth Month: ", default="January")
+        start_menu.add.button("Quit", pygame_menu.events.EXIT)
+        start_menu.mainloop(self.screen)
+
         pygame.display.flip()
+
 
     def get_birthday(self):
         month = (input("What's your birthday month (january, february, etc.): "))
         # testing the entry if it is name of a month
         month_list = ["january", "february", "march", "april", "may", "june","july", "august", "september", "october", "november", "december"]
-        while month not in month_list :
+        while month not in month_list:
                 print("You were supposed to enter the name of one of the 12 months. Try again:")
                 month = (input("What's your birthday month (january, february, etc.): "))
         day = int(input("What's your birthday day: "))
@@ -39,10 +50,20 @@ class Controller:
         return month, day
     
     def mainloop(self):
+
+        # while True:
+        #     if self.state == "START":
+        #         self.startloop()
+        #     elif self.state == "INPUT":
+        #         self.inputloop()
+        #     elif self.state =="OUTPUT":
+        #         self.outputloop()
+        
+        # Previous code
         print("checkpoint -2")
         controller = Controller()
         print("checkpoint -1")
-        [month, day] = controller.get_birthday()
+        [month, day] = self.get_birthday()
         print(month, day)
         print("checkpoint 0")
         user = User(month, day)
@@ -54,13 +75,33 @@ class Controller:
         proxy = Proxy()
         sign_info = proxy.get_sign_info(user_zodiac)
         print(sign_info)
+
         return sign_info
+    
+    # def startloop(self):
+        
+    #     self.menu = pygame_menu.Menu("Read your horoscope for today", width=400, height=300, theme=pygame_menu.themes.THEME_SOLARIZED)
+    #     self.menu.add.button("Play", self.get_input)
+    #     self.menu.add.button("Quit", pygame_menu.events.EXIT)
+        
+    #     while self.state == "START":
+    #         for event in pygame.event.get():
+    #             if event.type == pygame.MOUSEBUTTONDOWN:
+    #                 self.state == "INPUT"
 
-# Testing the mainloop() method; when testing only controller.py need to remove src. from the imports
-# controller = Controller()
-# controller.mainloop()
+    #     self.menu.update(pygame.event.get())
+        
+    #     self.menu.draw(self.screen)
+    #     pygame.display.flip()
 
+    
+    # def inputloop(self):
+    #     pass
 
-# in a method in Controller class
-# p = Proxy()
-# horo = p.get_horo()
+    # def outputloop(self):
+    #     pass
+        
+
+# For testing mainloop() method within controller.py; when testing only controller.py need to remove src. from the imports
+controller = Controller()
+controller.mainloop()
